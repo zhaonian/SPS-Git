47668d6d09f6227afe1672f129f481f3f54dd403
Zhaonian Luan <zluan@google.com>
2021-03-05 14:57:49 -0800

    add controller for welcome page

---
+++ WelcomeController.java
@@ -0,0 +1,11 @@
+@WebServlet("/welcome")
+public class TestServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    List<String> listofmessage = new ArrayList<String>();
+    listofmessage.add("Hello!");
+    listofmessage.add("Welcome to my Website!");
+    listofmessage.add("Hope you enjoy your stay!");
+  }
+}
+
