f99a2c753605adef4f06fdbe51901a6b07d7c763
Zhaonian Luan <zluan@google.com>
2021-03-05 15:37:20 -0800

    rename to service servelet

--- WelcomeController.java
+++ WelcomeController.java
@@ -1,5 +1,5 @@
 @WebServlet("/welcome")
-public class TestServlet extends HttpServlet {
+public class WelcomeServiceServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     List<String> listofmessage = new ArrayList<String>();
